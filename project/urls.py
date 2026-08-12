bb7268425f2fe4060707294b6da6d3c896f68af2
Letícia Dornas <letaraujo0108@gmail.com>
2026-08-12 09:14:41 -0300

    app hello

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helo/', include("helo.urls")),
+    path('hello/', include("hello.urls")),
 ]
